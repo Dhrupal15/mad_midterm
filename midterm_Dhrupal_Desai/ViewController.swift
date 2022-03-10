af23708ded4d66a34642434acf31eb6347a01386
Dhrupal Kamlesh Desai <desai.dhrupal15@gmail.com>
2022-03-10 09:15:08 -0500

    Initial Commit

---
+++ midterm_Dhrupal_Desai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  midterm_Dhrupal_Desai
+//
+//  Created by user204862 on 3/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ midterm_Dhrupal_DesaiTests/midterm_Dhrupal_DesaiTests.swift
@@ -0,0 +1,33 @@
+//
+//  midterm_Dhrupal_DesaiTests.swift
+//  midterm_Dhrupal_DesaiTests
+//
+//  Created by user204862 on 3/10/22.
+//
+
+import XCTest
+@testable import midterm_Dhrupal_Desai
+
+class midterm_Dhrupal_DesaiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
